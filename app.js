2bb41fbddf5329a2ea15d290c677eae42080a17d
Aniket <freakyyo26@gmail.com>
2020-12-05 21:32:21 +0530

    EJS implementation

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 // app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +19,7 @@ app.use("/admin", routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "Page Not Found" });
 });
 
 app.listen(3000);
