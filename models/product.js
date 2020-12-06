87b0b33c597fec1b12d3bdc114760766bb06dde2
Aniket <freakyyo26@gmail.com>
2020-12-06 13:26:09 +0530

    Refractoring

--- models/product.js
+++ models/product.js
@@ -1,28 +1,36 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, data) => {
-    if (err) cb([]);
-    else cb(JSON.parse(data));
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
     });
   }
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,18 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+
 const router = express.Router();
-const { getAddProduct, postAddProduct } = require("../controllers/products");
 
-router.get("/add-product", getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post("/add-product", postAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-exports.routes = router;
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+module.exports = router;
